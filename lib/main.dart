86a64418b166b21085019335596381006359ce40
Euclid Stellar <euclidstellar@gmail.com>
2023-10-21 23:47:15 +0530

    API mil gyi 😭

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ void main() {
   runApp(const MusicApp());
 }
 
+// key lelo bhai :- 6a506bf363msh70b280c80245a2ap15b295jsn61aff7c2dc6e
+// URL :- https://spotify23.p.rapidapi.com/search/?
+// queries apne according lele bhai 
 
 class MusicApp extends StatelessWidget {
   const MusicApp({super.key});
