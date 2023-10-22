14fc5decc656f90a4969e7cc5a88fcba66871937
Euclid Stellar <euclidstellar@gmail.com>
2023-10-22 19:15:19 +0530

    kaam chlu hai

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -19,6 +19,11 @@
 //   }
 // }
 
+// to fetch :-
+// albums
+// artists
+// playlists
+
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -26,84 +31,66 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     const border = OutlineInputBorder(
-        
         borderSide: BorderSide(
           color: Color.fromRGBO(225, 225, 225, 1),
         ),
         borderRadius: BorderRadius.horizontal(
-          left: Radius.circular(50), right: Radius.circular(50) ,
+          left: Radius.circular(50),
+          right: Radius.circular(50),
         ));
 
     return const Scaffold(
       //backgroundColor: Colors.black,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-
-              Center(
-               child: Text(
-                'Xerol',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-             
-                  
-             
-                   fontSize: 42,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    'Xerol',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 42,
                       fontWeight: FontWeight.bold,
                       color: Color.fromARGB(255, 35, 209, 228),
-                  
-                  
+                    ),
+                  ),
                 ),
-               ),
-             ),
-
-              SizedBox(
-              height: 30,
-             ),
-
-             
-              Text(
-                'Hi there..\nEuclid',
-                textAlign: TextAlign.start,
-                style: TextStyle(
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Hi there..\nEuclid',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
                     fontSize: 27,
                     fontWeight: FontWeight.bold,
-                    color: Colors.white70,),
-              ),
-
-              // SizedBox(
-
-              //   height: 20 ,
-
-              // ),
-
-              Padding(
-                padding: EdgeInsets.only(  top: 15 , bottom: 15 ,),
-                child: Expanded(
-                   
+                    color: Colors.white70,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 15,
+                    bottom: 15,
+                  ),
+                  child: Expanded(
                     child: TextField(
-                      
-
-                     
                       decoration: InputDecoration(
                         border: border,
-                        enabledBorder:
-                            border, 
-                        focusedBorder:
-                            border, 
+                        enabledBorder: border,
+                        focusedBorder: border,
                         prefixIcon: Icon(Icons.search),
                         hintText: 'Search',
                       ),
                     ),
                   ),
-              ),
-             
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
