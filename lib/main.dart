2178b27d014a8a43f7333a2c2736446d4c5442bc
Euclid Stellar <euclidstellar@gmail.com>
2023-10-22 11:10:06 +0530

    kuch to banaya hai !

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 import 'package:music_app/screens/welcome.dart';
 
 void main() {
@@ -8,6 +10,8 @@ void main() {
 // key lelo bhai :- 6a506bf363msh70b280c80245a2ap15b295jsn61aff7c2dc6e
 // URL :- https://spotify23.p.rapidapi.com/search/?
 // queries apne according lele bhai 
+
+
 
 class MusicApp extends StatelessWidget {
   const MusicApp({super.key});
@@ -19,8 +23,13 @@ class MusicApp extends StatelessWidget {
       title: 'Xerol',
       theme: ThemeData(
        // backgroundColor: colo,
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        scaffoldBackgroundColor: Colors.black,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
         useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: const WelcomeScreen()
     );
--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,19 +1,111 @@
+// import 'package:flutter/material.dart';
+
+// class WelcomeScreen extends StatefulWidget {
+//   const WelcomeScreen({super.key});
+
+//   @override
+//   State<WelcomeScreen> createState() => _WelcomeScreenState();
+// }
+
+// class _WelcomeScreenState extends State<WelcomeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       body: Container(
+
+//       )
+//     );
+//   }
+// }
 
 import 'package:flutter/material.dart';
 
-class WelcomeScreen extends StatefulWidget {
+class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
-  State<WelcomeScreen> createState() => _WelcomeScreenState();
-}
-
-class _WelcomeScreenState extends State<WelcomeScreen> {
-  @override
   Widget build(BuildContext context) {
+
+    const border = OutlineInputBorder(
+        
+        borderSide: BorderSide(
+          color: Color.fromRGBO(225, 225, 225, 1),
+        ),
+        borderRadius: BorderRadius.horizontal(
+          left: Radius.circular(50), right: Radius.circular(50) ,
+        ));
+
     return const Scaffold(
-      body: Center(
-        child: Text('Welcome'),
+      //backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Center(
+               child: Text(
+                'Xerol',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+             
+                  
+             
+                   fontSize: 42,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 35, 209, 228),
+                  
+                  
+                ),
+               ),
+             ),
+
+              SizedBox(
+              height: 30,
+             ),
+
+             
+              Text(
+                'Hi there..\nEuclid',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,),
+              ),
+
+              // SizedBox(
+
+              //   height: 20 ,
+
+              // ),
+
+              Padding(
+                padding: EdgeInsets.only(  top: 15 , bottom: 15 ,),
+                child: Expanded(
+                   
+                    child: TextField(
+                      
+
+                     
+                      decoration: InputDecoration(
+                        border: border,
+                        enabledBorder:
+                            border, 
+                        focusedBorder:
+                            border, 
+                        prefixIcon: Icon(Icons.search),
+                        hintText: 'Search',
+                      ),
+                    ),
+                  ),
+              ),
+             
+            ],
+          ),
+        ),
       ),
     );
   }
