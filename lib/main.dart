899e61b804dbf21fbd80a3787a8ae991b6348fcb
Euclid Stellar <euclidstellar@gmail.com>
2023-10-25 21:34:36 +0530

    modiefied

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 //import 'package:google_fonts/google_fonts.dart';
-import 'package:music_app/screens/welcome.dart';
+import 'package:music_app/screens/homescreen.dart';
 
 void main() {
   runApp(const MusicApp());
