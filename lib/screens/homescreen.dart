cdfeb3cce14b1a8dbf8fc0751806e9c71d54cff0
Euclid Stellar <euclidstellar@gmail.com>
2023-10-25 21:37:13 +0530

    screens folder created

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,142 @@
+// to fetch :-
+// albums
+// artists
+// playlists
+
+//import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:music_app/custom_widget/album.dart';
+import 'package:music_app/custom_widget/artist.dart';
+import 'package:music_app/custom_widget/playlist.dart';
+//import 'package:http/http.dart' as http;
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      //backgroundColor: Colors.black,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    'Xerol',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 35, 209, 228),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Hi there..\nEuclid',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                  ),
+                ),
+                // Search(border: border, onSearch: (String ) {  },),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Artists',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Artists(),
+                      Artists(),
+                      Artists(),
+                      Artists(),
+                      Artists(),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Albums',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Albums(),
+                      Albums(),
+                      Albums(),
+                      Albums(),
+                      Albums(),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Playlists',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Playlist(),
+                      Playlist(),
+                      Playlist(),
+                      Playlist(),
+                      Playlist(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
