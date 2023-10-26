beba2f3e294a24078eebb83c08730b394af347a3
Euclid Stellar <euclidstellar@gmail.com>
2023-10-26 09:51:20 +0530

    in maing of ui

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -141,9 +141,53 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
 
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: [
+//             Color.fromARGB(255, 65, 191, 225).withOpacity(0.8),
+//             Color.fromARGB(255, 198, 232, 235).withOpacity(0.8),
+//           ],
+//           begin: Alignment.topCenter,
+//           end: Alignment.bottomCenter,
+//         ),
+//       ),
+//       child: Scaffold(
+//         backgroundColor: Colors.transparent,
+//         appBar: const _CustomAppbar(),
+//         body: Container(
 
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class _CustomAppbar extends StatelessWidget with PreferredSizeWidget {
+//   const _CustomAppbar({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       backgroundColor: Colors.transparent,
+//       elevation: 0,
+
+//     );
+//   }
+
+//   @override
+
+//   Size get preferredSize => const Size.fromHeight(56);
+// }
 
 import 'package:flutter/material.dart';
 
@@ -156,22 +200,89 @@ class HomeScreen extends StatelessWidget {
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Color.fromARGB(255, 35, 209, 228).withOpacity(0.8),
-            Color.fromARGB(255, 230, 243, 244).withOpacity(0.8),
+            const Color.fromARGB(255, 21, 234, 92).withOpacity(0.8),
+            const Color.fromARGB(255, 4, 4, 4).withOpacity(0.8),
           ],
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
         ),
       ),
-      child: Scaffold( 
+      child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-    
-        ),
-        body: Container(
-    
-        ),
+        appBar: const _CustomAppbar(),
+        bottomNavigationBar: const _CustomBottomNavigationbar(),
+        body: Container(),
+
       ),
     );
   }
+}
+
+class _CustomBottomNavigationbar extends StatelessWidget {
+  const _CustomBottomNavigationbar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor:  const Color.fromARGB(255, 4, 4, 4).withOpacity(0.2),
+      unselectedItemColor: Colors.white,
+      selectedItemColor: Colors.white,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      elevation: 0,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_outline),
+          label: 'Favourites',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.play_circle_outline),
+          label: 'Play',
+        ),
+         BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
+
+class _CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const _CustomAppbar({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () {},
+        icon: const Icon(
+          Icons.grid_view_rounded,
+          color: Colors.white,
+        ),
+      ),
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(right: 16),
+          child: const CircleAvatar(
+            radius: 20,
+            backgroundImage: AssetImage('assets/images/check.JPG'),
+          
+          )
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56.0);
 }
