9a5e3e89519836de3ed60043c22453d6c9afa545
Euclid Stellar <euclidstellar@gmail.com>
2023-10-26 21:22:37 +0530

    song screen created

---
+++ lib/screens/song_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SongScreen extends StatelessWidget {
+  const SongScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Song'),
+      ),
+      body: const Center(
+        child: Text('Song Screen'),
+      ),
+    );
+  }
+}
