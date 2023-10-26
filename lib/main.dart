c8b44beb9643f040e05403670b84641e47d9d0a5
Euclid Stellar <euclidstellar@gmail.com>
2023-10-26 08:59:02 +0530

    dependencies added main.dart file modiefied

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:music_app/custom_widget/album.dart';
+import 'package:music_app/custom_widget/artist.dart';
+import 'package:music_app/custom_widget/playlist.dart';
 //import 'package:google_fonts/google_fonts.dart';
 import 'package:music_app/screens/homescreen.dart';
 
@@ -24,20 +29,28 @@ class _MusicAppState extends State<MusicApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Xerol',
       theme: ThemeData(
        // backgroundColor: colo,
         textTheme: GoogleFonts.latoTextTheme(
-          Theme.of(context).textTheme,
+          Theme.of(context).textTheme.apply(
+                bodyColor: Colors.white,
+                displayColor: Colors.white,
+                ),
         ),
         scaffoldBackgroundColor: Colors.black,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         useMaterial3: true,
         primarySwatch: Colors.amber,
       ),
-      home: const WelcomeScreen()
+      home: const WelcomeScreen(),
+      getPages: [
+        GetPage(name: '/home', page: () => const WelcomeScreen()),
+        GetPage(name: '/playlist', page: () => const Playlist()),
+        GetPage(name: '/album', page: () => const Albums()),
+        GetPage(name: '/artist', page: () => const Artists()),],
     );
   }
 }
